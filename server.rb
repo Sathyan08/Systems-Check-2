68cfd5150816432e0f560e0b311344c734baa172
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-08-23 20:22:08 -0400

    This commit adds the leaderboard page for the leaderboard link.

--- server.rb
+++ server.rb
@@ -9,7 +9,7 @@ def pull_game_data(filename)
 
   game_data = []
 
-  CSV.foreach(filename, headers:true) do |row|
+  CSV.foreach(filename, {headers:true, :header_converters => :symbol, :converters => :all}) do |row|
     game_data<< row
   end
 
@@ -21,8 +21,8 @@ def extract_team_names(game_data)
   team_names = []
 
   game_data.each do |game|
-    team_names << game["home_team"]
-    team_names << game["away_team"]
+    team_names << game[:home_team]
+    team_names << game[:away_team]
   end
 
   team_names.uniq
@@ -34,7 +34,7 @@ def pull_team_data(filename,team_name)
 
   team_data = []
 
-  CSV.foreach(filename, {headers:true, header_converters: symbol, converters:all}) do |row|
+  CSV.foreach(filename, {headers:true, header_converters: :symbol, converters: :all}.merge) do |row|
     if row["home_team"] == team_name || row["away_team"] == team_name
       team_data<< row
     end
@@ -43,9 +43,12 @@ def pull_team_data(filename,team_name)
   team_data
 end
 
-def create_leader_board
+def create_leader_board(game_data,team_names)
 
   leaderboard = []  ### this will be an array of hashes
+
+  wins = {}
+  loses = {}
 
   team_names.each do |team_name|
     wins[team_name] = 0
@@ -53,25 +56,59 @@ def create_leader_board
   end
 
   game_data.each do |game|
-    if game["home_score"] > game["away_score"]
-      wins["home_team"] += 1
-      loses["away_team"] += 1
+
+    # puts "game is #{game}"
+
+    if game[:home_score] > game[:away_score]
+      wins[game[:home_team]] += 1
+      loses[game[:away_team]] += 1
     else
-      wins["away_team"] += 1
-      loses["home_team"] += 1
+      wins[game[:away_team]] += 1
+      loses[game[:home_team]] += 1
     end
+  end
+    wins_sorted = wins.sort_by {|team_name, wins| wins}
+    wins_sorted.reverse!
 
-    wins.sort_by! {|team_name, wins| wins}
-    wins.reverse!
-
-    # loses.sort_by! {|team_name, loses| loses}
-
-  wins.each do |team_name, win_number|
-    team_hash[:team_name] = team_name
-    team_hash[:wins] = win_number
+  wins_sorted.each do |team_name, wins_number|
+    leaderboard_position = {}
+    leaderboard_position[:name] = team_name
+    leaderboard_position[:wins] = wins_number
+    leaderboard_position[:loses] = loses[team_name]
+    leaderboard << leaderboard_position
   end
 
+  leaderboard_copy = leaderboard
 
+  wins_groups = []
+
+  leaderboard_copy.each do |copy_position|
+    wins_group = []
+    wins_group << copy_position[:wins]
+    wins_groups << wins_group
+  end
+
+  wins_groups.uniq!
+
+  wins_groups.each do |wins_group|
+    leaderboard.each do |leaderboard_position|
+      if leaderboard_position[:wins] == wins_group[0]
+        wins_group << leaderboard_position
+      end
+    end
+  end
+
+final_leaderboard = []
+
+  wins_groups.each do |wins_group|
+    wins_group.shift
+    wins_group.sort_by! {|leaderboard_position| leaderboard_position[:loses]}
+    until wins_group.length == 0
+      final_leaderboard << wins_group.shift
+    end
+  end
+
+final_leaderboard
 
 end
 
@@ -90,6 +127,8 @@ end
 
 get '/leaderboard' do
   @game_data = pull_game_data("game_data.csv")
+  @team_names = extract_team_names(@game_data)
+  @leaderboard = create_leader_board(@game_data,@team_names)
 
   erb :leaderboard
 end
